bc82fe171628342977fb5195cf2865c4c288f189
mekladious <miraekladious@gmail.com>
2019-08-20 17:25:55 +0200

    slot machine randomizer

--- src/Components/Randomizer4/Randomizer4.jsx
+++ src/Components/Randomizer4/Randomizer4.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import RandomWheel from 'react-random-wheel';
+import Slot from 'react-slot-machine';
+import Fade from "react-reveal/Fade";
 
 class Randomizer4 extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            segments:[
-                {
-                    name: 'Red',
-                    color: 'red'
-                },
-                {
-                    name: 'Green',
-                    color: 'green'
-                },
-                {
-                    name: 'Blue',
-                    color: 'blue'
-                },
+            list:[
+                "Science",
+                "Sports", 
+                "World",
+                "History",
+                "Medicine",
+                "Music",
+                "Intelligence"
             ]
         }
         this.onSpinComplete = this.onSpinComplete.bind(this);
@@ -28,22 +24,35 @@ class Randomizer4 extends React.Component {
 	}
 
     render() {
-        let wheel;
-        const onSpinClick = () => {
-            wheel.spinWheel();
-        };
         return (
-            <div>
-                <RandomWheel
-                    segments={this.state.segments}
-                    onComplete={this.onSpinComplete}
-                    ref={( wheelRef ) => {
-                        wheel = wheelRef;
-                    }}
-                />
-                <button onClick={onSpinClick}>
-                    Spin
-                </button>
+            <div className="container">
+                <div className="row padded-row">
+                    <div className="col-4">
+                        <img
+                        className="template-logo"
+                        alt=""
+                        src={require("../../img/logo.png")}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <Fade left>
+                        <div className="col-2" />
+                        <div className="col-8 settings-container">
+                            <Slot target={"1"} duration={"3000"} times={"2"} turning={true}>
+                            {
+                                this.state.list.map(value =>
+                                <div style={{ width: '100%', height: '100%' }}>
+                                    {value}
+                                </div>
+                                )
+                                // Children of `Slot` be sure to be `width` and `height` are 100%.
+                            }
+                            </Slot>
+                        </div>
+                        <div className="col-2" />
+                    </Fade>
+                </div>
             </div>
             
         );
